854db0b60f50a9a5502be42b59f8269179115c32
calebpenning <calebpenning@mail.com>
2022-01-07 13:26:04 -0800

    added query and limit parameters to get function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
-
+const assert = require('assert')
 const circulationRepo = require("./repos/circulationRepo")
 const data = require('./circulation.json')
 
@@ -10,11 +10,32 @@ async function main() {
     const client = new MongoClient(url)
     await client.connect()
 
-    const results = await circulationRepo.loadData(data)
-    console.log(results.insertedCount, results.ops)
-    const admin = client.db(dbName).admin()
-    // console.log(await admin.serverStatus())
-    console.log(await admin.listDatabases())
+    try {
+        const results = await circulationRepo.loadData(data)
+        assert.equal(data.length, results.insertedCount)
+    
+        const getData = await circulationRepo.get()
+        assert.equal(data.length, getData.length)
+
+        const filterData = await circulationRepo.get({ Newspaper: getData[4].Newspaper })
+        assert.deepEqual(filterData[0], getData[4])
+
+        const limitData = await circulationRepo.get({}, 3)
+        assert.equal(limitData.length, 3)
+    }
+
+    catch(err) {
+        console.log(err)
+    }
+
+    finally {
+        const admin = client.db(dbName).admin()
+
+        await client.db(dbName).dropDatabase()
+        console.log(await admin.listDatabases())
+
+        client.close()
+    }
 }
 
 main()
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -3,7 +3,27 @@ const { MongoClient } = require('mongodb')
 function circulationRepo() {
     const url = "mongodb://localhost:27017"
     const dbName = "circulation"
-    
+
+    function get(query, limit) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url)
+            try {
+                await client.connect()
+                const db = client.db(dbName)
+
+                let items = db.collection('newspapers').find(query)
+
+                if (limit > 0) items = items.limit(limit)
+
+                resolve(await items.toArray())
+                client.close()
+            }
+            catch (err) {
+                reject(err)
+            }
+        })
+    }
+
     function loadData(data) {
         return new Promise(async (resolve, reject) => {
             const client = new MongoClient(url)
@@ -13,6 +33,7 @@ function circulationRepo() {
 
                 const results = await db.collection('newspapers').insertMany(data)
                 resolve(results)
+                client.close()
             }
 
             catch(err) {
@@ -20,7 +41,7 @@ function circulationRepo() {
             }
         })
     }
-    return { loadData }
+    return { loadData, get }
 }
 
 module.exports = circulationRepo()
